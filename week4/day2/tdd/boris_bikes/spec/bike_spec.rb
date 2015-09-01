8144b3c2fa935182f78bf48ae79f8c5253fd3a1e
Joshua Swiss <swissjoshua@gmail.com>
2015-09-01 14:24:28 +0100

    docking station test dock accepting bikes

--- week4/day2/tdd/boris_bikes/spec/bike_spec.rb
+++ week4/day2/tdd/boris_bikes/spec/bike_spec.rb
@@ -2,22 +2,21 @@ require_relative '../lib/bike'
 
 #we're describing the functionality of the specific class, Bike
 describe Bike do
+
+  let (:bike) { Bike.new }
   
   it 'should not be broken after we create it' do
-    bike = Bike.new #instantiate an instance of the Bike class
 
     expect(bike.broken?).to be false
   end
 
   it 'should be able to break' do
-    bike = Bike.new
     bike.break
     
     expect(bike.broken?).to be true
   end
 
   it 'should be fixable' do
-    bike = Bike.new
     bike.break
     bike.fix
 
